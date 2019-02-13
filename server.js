fa03a181eab91de101282828e199b27407c87073
Andi <ashores_6975@email.ric.edu>
2019-02-13 17:03:28 -0500

    made it pretty uwu

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ var http = require('http'),
 	N = 128;
     
 var express = require('express');
-var cors = require('cors');
+// var cors = require('cors');
 var app = express();
-app.use(cors());
+// app.use(cors());
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Credentials", "true")
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+});
 	
 var mscon = mysql.createConnection({
 	host: "localhost",
@@ -18,7 +24,9 @@ var mscon = mysql.createConnection({
 });
 mscon.connect(function(err) {
 	// if(err) throw err;
-	if(err) console.error(err); //it's a pain having it refuse to work unless the mysql server is running
+    //it's a pain having it refuse to work unless the mysql server is running
+    // if(err){ console.error(err); console.info('could not connect to mysql server, is it running?'); }
+    if(err){ console.info('could not connect to mysql server, is it running?'); }
 }); 
 
 http.createServer(function(request, response){
@@ -26,66 +34,81 @@ http.createServer(function(request, response){
 	if(path=="/getboard"){
         let params = url.parse(request.url, true).query;
         let name = (params.name || 'default').replace(/[/\\\.]/g,'');
-		console.log("request for a board state recieved");
+		console.log("request for "+name+" board state recieved");
 		// var data = fs.readFileSync("./board.txt");
 		var data = fs.readFileSync("./boards/"+name+".txt");
+		// var data = json.parse( fs.readFileSync("./boards/"+name+".txt") );
 		// console.log("Synchronous read: \n" + data.toString());
-		// response.setContentType("text/plain"); 
-		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.writeHead(200, {"Content-Type": "text/json"});
 		response.writeHead(200, {"Access-Control-Allow-Origin": "*" }); 
 		response.end(data);
 		console.log("data sent");
 	}
 	else if(path=="/changeboard"){
-		console.log("update for board state recieved");
 		let params = url.parse(request.url, true).query;
+        let name = (params.name || 'default').replace(/[/\\\.]/g,'');
+		console.log("change for "+name+" board state recieved");
 		// console.log(params);
 		let x = parseInt(params.x);
 		let y = parseInt(params.y);
-        let name = (params.name || 'default').replace(/[/\\\.]/g,'');
 		let state = (params.state == "true");
 		console.log("changing cell at x="+x+",y="+y+" to "+state);
-		let data = fs.readFileSync('boards/'+name+'.txt').toString('UTF-8');
+		let data = JSON.parse( fs.readFileSync('./boards/'+name+'.txt').toString('UTF-8') );
 		// console.log(data);
-		let lines = data.replace(/^\s+/,"").replace(/ /,".").split(/[\n\r,/]/);
+		// let lines = data.state.replace(/^\s+/g,"").replace(/ /g,".").replace(/[\n\r]/g,"/").split("/");
+		let lines = data.state;
 		while(lines[y].length < N){ lines[y] = lines[y] + '.'; } ////
 		
 		lines[y] = lines[y].slice(0,x) + (state ? 'O' : '.') + lines[y].slice(x+1);
 		while(lines[y].length < N){ lines[y] = lines[y] + '.'; }
+        while(lines.length < N){ lines = lines + [ lines[lines.length-1] ]; }
 		
-		fs.writeFileSync('./boards/'+name+'.txt', lines.join('\n'));
+		// fs.writeFileSync('./boards/'+name+'.txt', lines.join('\n'));
+        data.state = lines;
+		fs.writeFileSync('./boards/'+name+'.txt', JSON.stringify(data));
 		console.log("new state saved");
 		
 		//@TODO! update schema to support multiple boards
 		mscon.query('INSERT INTO changes(x,y,ts,ns) VALUES('+x+','+y+','+Date.now()+','+state+')',
 		  function (err, result, fields) {
-			if (err) throw err;
-			console.log(result);
+			// if (err){console.error(err);}
+			if (err){console.error('could not log change to mysql server, is it running?');}
+			// console.log(result);
 		});
 	
 		
-		// response.setContentType("text/plain"); 
-		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.writeHead(200, {"Content-Type": "text/json"});
 		response.writeHead(200, {"Access-Control-Allow-Origin": "*" }); 
-		response.end(lines.join('\n'));
+		// response.end(lines.join('\n'));
+		response.end(JSON.stringify(data));
 		console.log("data sent");
 	}
     else if(path=="/setboard"){
 		let params = url.parse(request.url, true).query;
         let name = (params.name || 'default').replace(/[/\\\.]/g,'');
-		console.log("update for board \""+name+"\" recieved");
+		console.log("update for "+name+" board state recieved");
         let body = '';
         request.on('data', chunk => {
             body += chunk.toString(); // convert Buffer to string
         });
-        // request.on('end', () => {
-            // response.end('ok');
-        // });
+        request.on('end', () => {
+            response.end('ok');
+            console.log('ok');
+            let data = JSON.parse(body);
+            if(data.survive === undefined){ data.survive = "23"; }
+            if(data.born    === undefined){ data.born    = "3"; }
+            // let data = JSON.parse(body);
+            // let state = data.state;
+            // let data = json.stringify( { "name":name,"survive":params.survive,"born":params.born,"state":body } );
+            fs.writeFileSync('./boards/'+name+'.txt', JSON.stringify(data));
+            console.log("new state saved for "+name+" (length "+body.length+")");
+        });
+        response.writeHead(200, {"Content-Type": "text/json"});
+		response.writeHead(200, {"Access-Control-Allow-Origin": "*" }); //trying to get cors to work
         
         // @todo figure out how to track this with db, and authentication???
 		
-		fs.writeFileSync('./boards/'+name+'.txt', body);
-		console.log("new state saved for "+name);
+		
 	}
 	else{
 		fs.readFile('./index.html', function(err, file) {  
@@ -98,4 +121,4 @@ http.createServer(function(request, response){
 		});
 	}
 }).listen(8001);
-console.log("server initialized");
+console.log("node server initialized");
